5e3602197a6098f784a3f30eec701ebb0e353874
clem <clemmartinez33240@gmail.com>
2023-05-11 09:16:08 +0200

    creation game's rules + styling

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -3,9 +3,18 @@ import { RxCross1 } from 'react-icons/rx';
 import { useGlobalContext } from '../context';
 
 const Rules = () => {
-  const {closeRules} = useGlobalContext();
+  const { closeRules } = useGlobalContext();
+
   return (
-    <div>Rules</div>
+    <div className='rules-wrapper center'>
+      <div className='rules flex-column'>
+        <RxCross1 className='close-rules' onClick={closeRules} />
+        <h1 className='rules-title'>Rules :</h1>
+        <h2 className='rules-instrucutons'>- Slect a grid size.</h2>
+        <h2 className='rules-instrucutons'>- Add living cells by clicking on grid's tiles.</h2>
+        <h2 className='rules-instrucutons'>- Click on 'Next' to display the next cells generation.</h2>
+      </div>
+    </div>
   )
 }
 
