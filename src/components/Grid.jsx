5ff0de90fa034fd91536babfb7a91b76c301c5e5
clem <clemmartinez33240@gmail.com>
2023-05-11 07:56:14 +0200

    creation 2d grid , algorithm + styling

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,27 +1,66 @@
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Tiles from './Tiles';
 import Error from '../subcomponents/Error';
-import { useGlobalContext} from '../context';
+import { useGlobalContext } from '../context';
 
 const Grid = () => {
- const { 
-        gridSize, 
-        setGridSize, 
-        newGridSize, 
-        setNewGridSize,
-        maxValue,
-        setMaxValue, 
-        dimensions, 
-        setDimensions, 
-        error, 
-        setError, 
-        showRules, 
-        ref, 
-         inputRef
-      } = useGlobalContext();
+  const {
+    gridSize,
+    setGridSize,
+    newGridSize,
+    setNewGridSize,
+    maxValue,
+    setMaxValue,
+    dimensions,
+    setDimensions,
+    error,
+    setError,
+    showRules,
+    ref,
+    inputRef
+  } = useGlobalContext();
+
+
+  const handleResizeClick = (e) => {
+    e.preventDefault();
+
+    if (inputRef.current.value > inputRef.current.max) {
+      setError(true);
+      setGridSize(maxValue);
+    } else {
+      setGridSize(newGridSize);
+      setError(false)
+    }
+  };
+
+
+  useEffect(() => {
+    if (window.innerWidth < 900) {
+      setMaxValue(400)
+    } else {
+      setMaxValue(500)
+    }
+  }, [window.innerWidth]);
+
+  useEffect(() => {
+    const containerHeight = ref.current.clientHeight - 16;
+    const conatinerWidth = ref.current.clientWidth - 16;
+    const tileContainerDimensions = { height: containerHeight, width: conatinerWidth }
+    setDimensions(tileContainerDimensions)
+  }, [gridSize]);
 
   return (
-    <div>Grid</div>
+    <div className='grid-wrapper flex-column'>
+      <div style={{ width: gridSize, height: gridSize }} ref={ref}>
+        <Tiles dimensions={dimensions} />
+        <div className={`${showRules ? 'hidden2' : 'hidden'}`} />
+      </div>
+      <div>{error && <Error />}</div>
+      <div>
+        <input type="number" value={newGridSize} onChange={(e) => setNewGridSize(parseInt(e.target.value))} min='300' max={maxValue} ref={inputRef} />
+        <button className='button buttonBS margin-left' onClick={handleResizeClick}>Resize Grid</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Tiles.jsx
+++ src/components/Tiles.jsx
@@ -1,22 +1,178 @@
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useGlobalContext } from '../context';
 
-const Tiles = ({dimensions}) => {
-    const {
-         tilesArr,
-         setTilesArr,
-         gridSize, 
-         clicker, 
-         setClicker, 
-         resetBtnClicked,
-         nextBtnClicked, 
-         playing, 
-         setPlaying, 
-         running 
-        } = useGlobalContext();
-        
+const Tiles = ({ dimensions }) => {
+  const {
+    tilesArr,
+    setTilesArr,
+    gridSize,
+    clicker,
+    setClicker,
+    resetBtnClicked,
+    nextBtnClicked,
+    playing,
+    setPlaying,
+    running
+  } = useGlobalContext();
+
+  const tileSize = 16;
+  const numOfRows = Math.floor(dimensions.height / tileSize);
+  const numOfColumns = Math.floor(dimensions.width / tileSize);
+
+  useEffect(() => {
+    const tiles = generateTiles()
+    setTilesArr([...tiles])
+  }, [gridSize, numOfRows, numOfColumns]);
+
+  useEffect(() => {
+    resetTiles()
+    setClicker(clicker ? false : true)
+  }, [resetBtnClicked]);
+
+  useEffect(() => {
+    const updatedTiles = updateTileState([...tilesArr])
+    setTilesArr(updatedTiles)
+  }, [nextBtnClicked]);
+
+  useEffect(() => {
+    play([...tilesArr])
+  }, [playing]);
+
+  const play = async (tiles) => {
+
+    if (running.current) {
+
+      setTilesArr([...updateTileState([...tiles])])
+      await wait(300)
+      setPlaying(playing === "yes" ? "no" : "yes")
+    }
+  };
+
+  const wait = (timeout) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, timeout)
+    })
+  };
+
+  useEffect(() => {
+    const updatedTiles = updateTileState([...tilesArr])
+    setTilesArr(updatedTiles)
+  }, []);
+
+  const generateTiles = () => {
+
+    const tiles = []
+
+    let key = 0
+
+    for (let y = 0; y < numOfRows; y++) {
+
+      const row = []
+
+      for (let x = 0; x < numOfColumns; x++) {
+
+        const tile = {
+          x,
+          y,
+          active: false,
+          key: `tile${key}`
+        }
+
+        key++
+        row.push(tile)
+      }
+
+      tiles.push(row)
+    }
+
+    return tiles
+  };
+
+  const resetTiles = () => {
+
+    const tiles = tilesArr
+
+    if (tiles[0] !== undefined) {
+      tiles.forEach((row) => {
+        row.forEach((tile) => {
+          if (tile.active) {
+            tile.active = false
+          }
+        })
+      })
+    }
+  };
+
+  const updateTileState = (tiles) => {
+
+    let updatedStateTiles = JSON.parse(JSON.stringify(tiles))
+    let numOfAdjacentActiveTiles = 0
+
+    if (tiles !== undefined) {
+
+      for (let row = 0; row < numOfRows; row++) {
+
+        for (let col = 0; col < numOfColumns; col++) {
+
+          const tile = tiles[row][col]
+
+          for (let y = -1; y <= 1; y++) {
+
+            for (let x = -1; x <= 1; x++) {
+
+              const validRow = tiles.includes(tiles[row + y])
+
+              if (validRow) {
+
+                const validCol = tiles[row + y].includes(tiles[row + y][col + x])
+
+                if (validCol) {
+
+                  const currentTile = tiles[row + y][col + x]
+
+                  if (currentTile.active && currentTile !== tile) {
+
+                    numOfAdjacentActiveTiles += 1
+                  }
+                }
+              }
+            }
+          }
+
+          if ((tile.active && numOfAdjacentActiveTiles < 2) || (tile.active && numOfAdjacentActiveTiles > 3)) {
+            updatedStateTiles[row][col].active = false
+          }
+
+          if (!tile.active && numOfAdjacentActiveTiles === 3) {
+            updatedStateTiles[row][col].active = true
+          }
+
+          numOfAdjacentActiveTiles = 0
+        }
+      }
+    }
+
+    return updatedStateTiles
+  };
+
+  const toggleTile = (tile) => {
+
+    tile.active = tile.active ? false : true
+    setClicker(clicker ? false : true)
+  };
+
   return (
-    <div>Tiles</div>
+    <>
+      <div className="tiles centre">
+        {tilesArr.map((row) => {
+          return (
+            row.map((tile) => {
+              return <div key={tile.key} className={`tile ${tile.active ? "active" : "inactive"}`} style={{ width: tileSize + "px", height: tileSize + "px" }} onClick={() => { toggleTile(tile) }}></div>
+            })
+          )
+        })}
+      </div>
+    </>
   )
 }
 
