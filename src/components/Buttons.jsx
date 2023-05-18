57efdf2a88703ec0391f3f427940a6db3beb976d
clem <clemmartinez33240@gmail.com>
2023-05-18 11:49:12 +0200

    addition of stop btn

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useGlobalContext } from '../context';
-import { useRef, useState } from 'react';
+
 
 const Buttons = () => {
   const {
@@ -9,29 +9,23 @@ const Buttons = () => {
     nextBtnClicked,
     setNextBtnClicked,
     setPlaying,
-    playing,
     running,
   } = useGlobalContext();
 
-  const [startBtnText, setStartBtnText] = useState('Start');
-
 
   const resetTiles = () => {
-    setStartBtnText('Start');
     running.current = false
     setPlaying(false)
     setResetBtnClicked(resetBtnClicked ? false : true)
   };
 
   const nextBtnClick = () => {
-    setStartBtnText('Start');
     running.current = false
     setPlaying(false)
     setNextBtnClicked(nextBtnClicked ? false : true)
   };
 
   const startBtnClick = () => {
-    setStartBtnText('Stop');
     if (!running.current) {
       running.current = true
       setPlaying(true)
@@ -39,14 +33,16 @@ const Buttons = () => {
   };
 
   const stopBtnClick = () => {
-    setStartBtnText('Start');
     running.current = false;
     setPlaying(false)
+
   };
+
 
   return (
     <div className="center btn-container flex-row">
-      <button id="start-btn" className="button buttonBS " onClick={playing ? stopBtnClick : startBtnClick}>{startBtnText}</button>
+      <button id="start-btn" className="button buttonBS " onClick={startBtnClick}>Start</button>
+      <button id="start-btn" className="button buttonBS " onClick={stopBtnClick}>Stop</button>
       <button id="next-btn" className="button margin-left buttonBS" onClick={nextBtnClick}>Next</button>
       <button id="reset-btn" className="button margin-left buttonBS" onClick={resetTiles}>Reset</button>
     </div>
